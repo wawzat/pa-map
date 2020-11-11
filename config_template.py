0ca811bc89fcc82729392d086bef8c1a68508670
wawzat <wawzat@gmail.com>
2020-11-11 08:42:47 -0800

    Initial commit

---
+++ config_template.py
@@ -0,0 +1,15 @@
+#config.py template
+#Enter keys and directory path below and rename file config.py
+
+# PurpleAir Keys 
+READ_KEY = '<READ KEY>'
+WRITE_KEY = '<WRITE KEY>'
+
+# mapbox Tokens
+access_token = '<ACCESS TOKEN>'
+private_token = '<PRIVATE TOKEN>'
+
+
+#user_directory = r' '
+matrix5 = r'd:\Users\wawzat\OneDrive\Documents\House\PurpleAir'
+
