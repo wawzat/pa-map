82f7eef601c0c08aa65a4ed1216e5f391c2acf2e
wawzat <wawzat@gmail.com>
2020-12-07 07:59:59 -0800

    Corrected variable names and added comments.

--- config_template.py
+++ config_template.py
@@ -1,18 +1,21 @@
 #config.py template
 #Enter keys and directory path below and rename file config.py
 
-# PurpleAir Keys 
+# PurpleAir Keys - Replace <TEXT> with the key but keep the quotes, i.e., 'EF5C7BBE-BE54-22AE-873E-53320A400313' (note this is not a valid key)
+# Request keys from contact@purpleair.com.
 purpleair_read_key = '<READ KEY>'
 purpleair_write_key = '<WRITE KEY>'
 
 # mapbox Tokens
-purpleair_access_token = '<ACCESS TOKEN>'
-purpleair_private_token = '<PRIVATE TOKEN>'
+# Register an account with https://www.mapbox.com to get tokens
+mapbox_access_token = '<ACCESS TOKEN>'  #Default public token
+mapbox_private_token = '<PRIVATE TOKEN>' #Token you create.
 
 #user_directory = r' '
 root_path = r'd:\Users\wawzat\OneDrive\Documents\House\PurpleAir\pa_map_plot' #MATRIX5
 #root_path = r'c:\Users\wawzat\OneDrive\Documents\House\PurpleAir'            #servitor
 
+#These folders will become sub folders to the root path.
 video_folder = 'videos'
 images_folder = 'images'
 data_folder = 'Data'
