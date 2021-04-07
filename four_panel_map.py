c0064e3379dbffdc5a74868477bbef7988e73c2a
wawzat <wawzat@gmail.com>
2021-04-07 12:46:22 -0700

    Version before attempting multiple sub-plots

--- four_panel_map.py
+++ four_panel_map.py
@@ -16,7 +16,7 @@ from get_map import get_map
 
 
 def plot_map(root_path, df, map_plt, fig_num, start_time, bbox, label, range, marker, map):
-    print(df)
+    #print(df)
     if map == 'd':
         map_text_color = 'w'
     elif map == 'l':
@@ -40,7 +40,6 @@ def plot_map(root_path, df, map_plt, fig_num, start_time, bbox, label, range, ma
         cmap=cc.cm.fire_r,
         vmin=range[0],
         vmax=range[1])
-    #ax.scatter(df.Lon, df.Lat, zorder=1, alpha=0.8, c=df.Ipm25, s=18, cmap=cc.cm.fire, vmin=20, vmax=255)
     ax.set_title(label)
     ax.text(0.06, 0.06, str(start_time), fontsize=10, transform=ax.transAxes, color=map_text_color)
     ax.text(0.02, 0.02, mapbox_attribution, transform=ax.transAxes, color=map_text_color)
@@ -66,7 +65,7 @@ bbox_mapbox = (bbox[0]-.004, bbox[1]-.004, bbox[2]+.004, bbox[3]+.004)
 root_path = config.all_sensors_root_path
 images_folder = config.all_sensors_images_folder
 input_filename = 'combined_summarized_csv.csv'
-output_filename = '1_resample_df.csv'
+output_filename = 'resample_df.csv'
 input_full_file_path = root_path + os.path.sep + images_folder + os.path.sep + input_filename
 output_full_file_path = root_path + os.path.sep + images_folder + os.path.sep + output_filename
 map_filename = 'map_dark.png'
@@ -85,19 +84,21 @@ mapping = ({
             })
 df = df.rename(columns=mapping)
 df = df.groupby('Sensor')
-df2 = df.resample('Q-NOV', on='DateTime_US_Pacific').mean()
+df = df.resample('Q-NOV', on='DateTime_US_Pacific').mean()
 plot_start_time = datetime(2020, 5, 31)
 #plot_end_time = datetime(2020, 8, 31)
-df2.reset_index(inplace=True)
-df2 = df2[df2['DateTime_US_Pacific'] == plot_start_time]
-#df2 = df2[(df2['DateTime_US_Pacific'] >= plot_start_time) & (df2['DateTime_US_Pacific'] <= plot_end_time)]
-first_datetime = min(df2['DateTime_US_Pacific'])
+df.reset_index(inplace=True)
+#df = df[(df['DateTime_US_Pacific'] >= plot_start_time) & (df['DateTime_US_Pacific'] <= plot_end_time)]
+first_datetime = min(df['DateTime_US_Pacific'])
 #last_datetime = max(df['DateTime_US_Pacific'])
 start_time = first_datetime
 cols = ['Lon', 'Lat', 'Sensor', 'DateTime_US_Pacific', 'PM25']
-df2 = df2[cols]
-df2 = df2.dropna()
-df2['PM25'] = df2['PM25'].astype(int)
-#print(df2)
-df2.to_csv(output_full_file_path)
-fig_num = plot_map(root_path, df2, map_plt, 1, start_time, bbox_plot, 'NASA', range, marker, map)
+df = df[cols]
+df = df.dropna()
+#df['PM25'] = df['PM25'].astype(int)
+#print(df)
+df.to_csv(output_full_file_path)
+fig_num = 1
+for season in seasons:
+    df2 = df[df['DateTime_US_Pacific'] == season]
+    fig_num = plot_map(root_path, df2, map_plt, fig_num, start_time, bbox_plot, 'NASA', range, marker, map)
